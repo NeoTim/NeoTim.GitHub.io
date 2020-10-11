6c7d14bfae9a59dbb7518ba01f1c1ce31ac8300b
NeoTim <14356809+NeoTim@users.noreply.github.com>
2020-10-10 19:15:29 -0700

    Add

---
+++ docs/16/845.js
@@ -0,0 +1,1 @@
+{"name":"create Stored Procedures and virtual table in MySQL?  Because we have more than 500 lines of SQL select statements, it runs very slowly.","work":"t","_t":1558052882861,"_id":"RQM1Nq4ypY4y79hy","end":1558052882861,"ok":true}
