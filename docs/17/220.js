db3fc801a91a5ddedeb8a2e3912491602563d2a4
NeoTim <14356809+NeoTim@users.noreply.github.com>
2020-10-10 19:08:12 -0700

    Add

---
+++ docs/17/220.js
@@ -0,0 +1,1 @@
+{"name":"5 Edit   page removed ArticleType,Merchants,PublishDate fields, also deleted from the database. also added a new field here to be automatically updated when a modification is made to the article.","work":"t","_t":1588980393737,"_id":"3Cnm1nSJHIbsFnh1","assigner":"Ben","ok":true}
