1bd93ceacdd3b3c2987b169b0fb140088ccd1fc5
NeoTim <14356809+NeoTim@users.noreply.github.com>
2020-10-10 19:08:31 -0700

    Add

---
+++ docs/17/203.js
@@ -0,0 +1,1 @@
+{"name":"added [EDIT] button, click it to show Knowledge Base article in same page that can edit and make changes and save.","work":"t","_t":1587334361029,"_id":"BApcziq7O9q43tUx","assigner":"Ben","ok":true,"end":1587922376825}
