a70607a1187a2cc0e0d055abf0b23a08535d87f1
NeoTim <14356809+NeoTim@users.noreply.github.com>
2020-10-10 19:08:17 -0700

    Add

---
+++ docs/17/216.js
@@ -0,0 +1,1 @@
+{"name":"1. Pre-Sales page removed ArticleType,Merchants,PublishDate fields, also deleted from the database. also added a new field here to be automatically updated when a modification is made to the article:","work":"t","_t":1588980273966,"_id":"fO70VL8qKs2tdLLj","assigner":"Ben","ok":true}
