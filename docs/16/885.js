9d717f81d1648e2ea3ad984072a1dbbd2bb8eca3
NeoTim <14356809+NeoTim@users.noreply.github.com>
2020-10-10 19:14:42 -0700

    Add

---
+++ docs/16/885.js
@@ -0,0 +1,1 @@
+{"name":"再給我一份現在在google shopping 上面的 item list in Excel","work":"t","_t":1560039232838,"_id":"i582QWiRbHl6wU0z","end":1560039232838,"ok":true,"assigner":"Vincent","detail":"我早上把之前超過兩千磅的商品 exclude from Google Shopping 刪除了, 現在應該都傳到Google Merchant Center, 你再給我一份現在在oogle shopping 上面的 item list in Excel","wechat":true,"urgent":false,"email":true}
