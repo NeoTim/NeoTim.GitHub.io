0e91047f94f80704907fbb3313c6659c7db0f779
NeoTim <14356809+NeoTim@users.noreply.github.com>
2020-10-10 19:13:58 -0700

    Add

---
+++ docs/16/923.js
@@ -0,0 +1,1 @@
+{"name":"upload 24029 directly","work":"t","_t":1561401405923,"_id":"YtlXTOkm3xCERZh3","assigner":"Jason","detail":"select * from catalog_product_entity where tpin='3V3 '\n\nselect * from catalog_product_entity where entity_id=24029\n\n\nupdate catalog_product_entity set updated_at=now() where entity_id=24029","received":true,"ok":true,"end":1561401405923}
