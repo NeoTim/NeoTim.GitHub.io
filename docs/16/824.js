801b2f229f2a6eeeabe26003d12c5539abcaeb61
NeoTim <14356809+NeoTim@users.noreply.github.com>
2020-10-10 19:15:53 -0700

    Add

---
+++ docs/16/824.js
@@ -0,0 +1,1 @@
+{"name":"Create attribute: exclude_from_google_shopping","work":"t","_t":1557443918617,"_id":"hLeNwMLm0hMqyrJD","end":1557443918617,"detail":"left join catalog_product_entity_varchar exclude_from_google_shopping on exclude_from_google_shopping.attribute_id = 337 and exclude_from_google_shoppingk.entity_id = p.entity_id # exclude_from_google_shopping","ok":true}
