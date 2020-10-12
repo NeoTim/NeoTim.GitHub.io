26150b1a962efe7e2b7e2cfbb3ca5e431a27fe5f
NeoTim <14356809+NeoTim@users.noreply.github.com>
2020-10-12 13:53:58 -0700

    Change

--- docs/17/294.js
+++ docs/17/294.js
@@ -1,1 +1,1 @@
-{"id":17294,"date":"Sat Oct 10 2020","time":"2020-10-11T06:54:26.222Z","priority":"3","name":"need Send grid account and Email sender","assigner":"Jun"}
+{"id":17294,"date":"Sat Oct 10 2020","time":"2020-10-11T06:54:26.222Z","priority":"3","name":"need Send grid account and Email sender","assigner":"Jun","ok":true,"end":1602536038978}
