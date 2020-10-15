53865aee9ab2b315ae3ad4d4eb5138b406744640
NeoTim <14356809+NeoTim@users.noreply.github.com>
2020-10-15 16:04:49 -0700

    Change file

--- docs/17/299.js
+++ docs/17/299.js
@@ -1,1 +1,1 @@
-{"name":"Attached is the Ware2GO API Documentation and the stating environment tokens","id":17299,"work":"3","_t":1602464769461,"date":"Sun Oct 11 2020","time":"2020-10-12T01:07:24.463Z","priority":"3"}
+{"name":"Attached is the Ware2GO API Documentation and the stating environment tokens","id":17299,"work":"3","_t":1602464769461,"date":"Sun Oct 11 2020","time":"2020-10-12T01:07:24.463Z","priority":"3","file":"W2G API 2.8.pdf"}
