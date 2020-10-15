e276c94cb57c6861a07fd9351a3e248904b30e31
NeoTim <14356809+NeoTim@users.noreply.github.com>
2020-10-15 13:28:19 -0700

    Change

--- docs/17/297.js
+++ docs/17/297.js
@@ -1,1 +1,1 @@
-{"name":"ttt","id":17297,"_t":1602446906060,"date":"Sun Oct 11 2020","time":"2020-10-11T20:08:29.336Z","priority":"3","assigner":"Daniel","ok":true,"end":1602793693590}
+{"name":"ttt","id":17297,"_t":1602446906060,"date":"Sun Oct 11 2020","time":"2020-10-11T20:08:29.336Z","priority":"3","assigner":"Daniel","ok":false,"end":1602793693590}
