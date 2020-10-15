3982f4d6eb586beb595d109400b3242f56a481ee
NeoTim <14356809+NeoTim@users.noreply.github.com>
2020-10-15 09:45:24 -0700

    Change git

--- docs/17/291.js
+++ docs/17/291.js
@@ -1,1 +1,1 @@
-{"name":"Timeesheet Application。Tables: SELECT * FROM TimeSheet SELECT * FROM Employee","work":"1","_t":1601921456276,"_id":"Qpcaur9HF3ji9sv3","assigner":"Steve","id":17291,"date":"Thu Oct 15 2020","time":"2020-10-15T16:45:09.948Z","priority":"3"}
+{"name":"Timeesheet Application。Tables: SELECT * FROM TimeSheet SELECT * FROM Employee","work":"1","_t":1601921456276,"_id":"Qpcaur9HF3ji9sv3","assigner":"Steve","id":17291,"date":"Thu Oct 15 2020","time":"2020-10-15T16:45:09.948Z","priority":"3","git":"TimeSheet"}
