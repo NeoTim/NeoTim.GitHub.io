7e2e994febce240cd58b11e56ecb0715be7fdd45
NeoTim <14356809+NeoTim@users.noreply.github.com>
2020-10-18 11:54:57 -0700

    Change work

--- docs/17/305.js
+++ docs/17/305.js
@@ -1,1 +1,1 @@
-{"name":"你这个是从bt网站上拉下来的，还是从google adsense那边取的","id":17305,"work":1,"_t":1602950676552,"assigner":"Will"}
+{"name":"你这个是从bt网站上拉下来的，还是从google adsense那边取的","id":17305,"work":"1","_t":1602950676552,"assigner":"Will"}
