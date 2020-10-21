9bd20de84243c52a7d913603d9505977eabf3548
NeoTim <14356809+NeoTim@users.noreply.github.com>
2020-10-21 14:07:45 -0700

    Change work

--- docs/17/309.js
+++ docs/17/309.js
@@ -1,1 +1,1 @@
-{"name":"add the Ads to the bottom of the pages on both bt.com. Let me know if you have any question.","id":17309,"work":1,"_t":1603314443801,"assigner":"Jun"}
+{"name":"add the Ads to the bottom of the pages on both bt.com. Let me know if you have any question.","id":17309,"work":"1","_t":1603314443801,"assigner":"Jun"}
