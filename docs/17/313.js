9bffce97cb49ac04c3aa83781e0234ccefccf457
NeoTim <14356809+NeoTim@users.noreply.github.com>
2020-10-29 15:53:47 -0700

    Change

--- docs/17/313.js
+++ docs/17/313.js
@@ -1,1 +1,1 @@
-{"name":"We can’t ship to Canada anymore, we also change the 48 states as have ever requested.   How come we allow to ship to Canada? Also, we only allow to ship to 48 states in US. Correct?","id":17313,"work":1,"_t":1604011817267}
+{"name":"We can’t ship to Canada anymore, we also change the 48 states as have ever requested.   How come we allow to ship to Canada? Also, we only allow to ship to 48 states in US. Correct?","id":17313,"work":1,"_t":1604011817267,"assigner":"Jun"}
