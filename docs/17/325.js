60e8bda1d536e286576afe8dadb97ec9e6315e2d
NeoTim <14356809+NeoTim@users.noreply.github.com>
2020-11-21 11:22:31 -0800

    Change

--- docs/17/325.js
+++ docs/17/325.js
@@ -1,1 +1,1 @@
-{"name":"have you used Microsoft power BI? I think it is enough for us.","id":17325,"work":"1","_t":1605136687430,"assigner":"Jun"}
+{"name":"have you used Microsoft power BI? I think it is enough for us.","id":17325,"work":"1","_t":1605136687430,"assigner":"Jun","ok":true,"end":1605986554519}
