0e3361e118228bc29f3525b56a5eaf97eb72e696
NeoTim <14356809+NeoTim@users.noreply.github.com>
2020-12-09 15:18:56 -0800

    Change

--- docs/17/337.js
+++ docs/17/337.js
@@ -1,1 +1,1 @@
-{"name":"Please remove Quickspark section on finance page","id":17337,"work":"1","_t":1605988875579}
+{"name":"Please remove Quickspark section on finance page","id":17337,"work":"1","_t":1605988875579,"assigner":"Jun"}
