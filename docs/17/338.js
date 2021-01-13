ce90bb70efd0cb094d9f5f9af56a4d6c965dbbbf
NeoTim <14356809+NeoTim@users.noreply.github.com>
2021-01-13 15:52:38 -0800

    Change

--- docs/17/338.js
+++ docs/17/338.js
@@ -1,1 +1,1 @@
-{"name":"Just realized that we don’t currently have a high resolution image for Bolton on file.     Can you please send me one?","id":17338,"work":"1","_t":1607631389775,"assigner":"Jun","end":1607631389775}
+{"name":"Just realized that we don’t currently have a high resolution image for Bolton on file.     Can you please send me one?","id":17338,"work":"1","_t":1607631389775,"assigner":"Jun","end":1607631389775,"ok":true}
